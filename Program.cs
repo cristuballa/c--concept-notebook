6255f58d3d953187d74e9909ac52055580d923b0
crispintuballa <crispin.tuballa@scenicgroup.co.nz>
2022-09-16 16:02:43 +1200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+
+namespace programs
+{
+    class program
+    {
+        public static Mutex _mutex= new Mutex();
+        static void main()
+        {
+            for (int i=0; i<6; i++)
+            {
+                new Thread(Write).Start();
+            }
+            Console.Read();
+        }
+        
+        static void Write()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} waiting...");
+            _mutex.WaitOne();
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} writing...");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Writing completed..");
+            _mutex.ReleaseMutex();
+        }
+
+    }
+
+}
